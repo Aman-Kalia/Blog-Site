01133fb486e5d8e3483a122321f4bd6654f7f853
Aman Kalia <63893393+Aman-Kalia@users.noreply.github.com>
2022-02-26 11:26:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ app.use('/server/categories', categoryRoute);
 // }
 if(process.env.NODE_ENV=="production"){
     app.use(express.static("client/build"));
-    const path=require('path');
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
